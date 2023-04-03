2d36af80e027e68a63592c15ae2fbcb08fcfaf4b
maeng0830 <kmk930830@gmail.com>
2023-04-03 15:30:38 +0900

    Feat: create album project

---
+++ src/main/java/com/maeng0830/album/AlbumApplication.java
@@ -0,0 +1,13 @@
+package com.maeng0830.album;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maeng0830/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maeng0830.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
