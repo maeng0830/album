3305b422b740420d07111a5829fab8d8ce3b4065
maeng0830 <kmk930830@gmail.com>
2023-04-05 20:59:02 +0900

    Chore: develop 리포지토리 생성, 설정 데이터 생성

--- src/main/java/com/maeng0830/album/AlbumApplication.java
+++ src/main/java/com/maeng0830/album/AlbumApplication.java
@@ -2,8 +2,10 @@ package com.maeng0830.album;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/env.properties")
 public class AlbumApplication {
 
 	public static void main(String[] args) {
