51133293e2b5510caf3b0c8cd915248bcfabfcfe
maeng0830 <kmk930830@gmail.com>
2023-06-29 22:56:08 +0900

    Feat: index.html, index.js 구현 - ajax를 통해 피드 목록을 동적으로 가져올 수 있도록 구현

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,51 +1,63 @@
-// 첫 번째 페이지 가져오기
-$(document).ready(function () {
+let currentPage = 0;
 
-  var param = '?page=0&size=3';
+$(document).ready(getFeeds);
+
+document.querySelector('#loadMoreBtn').addEventListener('click', function () {
+  getFeeds();
+})
+
+function getFeeds() {
+
+  let param = '?page=' + currentPage + '&size=3';
+  currentPage +=1;
 
   $.ajax({
     type: 'GET',
     url: '/feeds' + param,
     success: function (feeds) {
       if (!feeds.length) {
-        document.querySelector('#feedList')
-            .innerHTML = '<div class="col-7 mb4">등록된 피드가 없습니다.</div>>'
+        alert('더 이상 등록된 피드가 없습니다.');
       } else {
         var feedHtml = '';
 
         feeds.forEach(feed => {
-          feedHtml += '<div class="col-7 mb-4">\n'
-              + '          <div class="card shadow-sm">\n'
-              + '            <img th:if="${feed.feedImages.size() ne 0}" th:src="${feed.feedImages[0].imagePath}" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Card Image" preserveAspectRatio="xMidYMid slice" focusable="false">\n'
-              + '            <img th:if="${feed.feedImages.size() eq 0}" src="#" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Card Image" preserveAspectRatio="xMidYMid slice" focusable="false">\n'
-              + '            <div class="card-body">\n'
-              + '              <h5 th:text="${feed.title}" class="card-title">카드 제목</h5>\n'
-              + '              <p  th:text="${feed.content}" class="card-text">카드 내용</p>\n'
-              + '              <div class="d-flex justify-content-between align-items-center">\n'
-              + '                <div class="btn-group">\n'
-              + '                  <a th:href="/member-view/${feed.createdById}" class="rounded-circle">\n'
-              + '                    <img th:if="${feed.createdByImage != null}" th:src="${feed.createdByImage}" alt="작성자 이미지" width="30" height="30">\n'
-              + '                    <img th:if="${feed.createdByImage == null}" src="#" alt="작성자 이미지" width="30" height="30">\n'
-              + '                  </a>\n'
-              + '                </div>\n'
-              + '                <div>\n'
-              + '                  <small th:text="댓글(${feed.commentCount})" class="text-muted">댓글</small>\n'
-              + '                  <br>\n'
-              + '                  <small th:text="${feed.createdAt}" class="text-muted">작성시간</small>\n'
-              + '                </div>\n'
-              + '              </div>\n'
-              + '            </div>\n'
-              + '          </div>\n'
-              + '        </div>';
+          var feedImage = feed.feedImages.length !== 0 ? feed.feedImages[0].imagePath : '#';
+          var createdByImage = feed.createdByImage != null ? feed.createdByImage : '#';
+
+          feedHtml += `
+            <div class="col-7 mb-4">
+              <div class="card shadow-sm">
+                <img src="${feedImage}" class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Card Image" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <div class="card-body">
+                  <h5 class="card-title">${feed.title}</h5>
+                  <p class="card-text">${feed.content}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <a href="/member-view/${feed.createdById}" class="rounded-circle">
+                        <img src="${createdByImage}" alt="작성자 이미지" width="30" height="30">
+                      </a>
+                    </div>
+                    <div>
+                      <small class="text-muted">댓글(${feed.commentCount})</small>
+                      <br>
+                      <small class="text-muted">${feed.createdAt}</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
         })
 
-        document.querySelector('#feedList').innerHTML = feedHtml;
+        if (currentPage === 1) {
+         document.querySelector('#feedList').innerHTML = feedHtml;
+        } else {
+          document.querySelector('#feedList').insertAdjacentHTML('beforeend', feedHtml);
+        }
       }
     },
     error: function () {
       alert("피드 목록을 가져오는데 실패했습니다.");
     }
   })
-})
-
-// 더보기 버튼으로 다음 페이지 가져오기
+}
