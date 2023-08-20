ea057148c0b22c21807adeda0678f2a1d6f8a703
maeng0830 <kmk930830@gmail.com>
2023-08-21 00:30:06 +0900

    Fix: local, aws 환경에 따른 이미지 url 설정

--- src/main/java/com/maeng0830/album/common/controller/CommonController.java
+++ src/main/java/com/maeng0830/album/common/controller/CommonController.java
@@ -1,10 +1,9 @@
 package com.maeng0830.album.common.controller;
 
-import com.maeng0830.album.common.filedir.FileDir;
+import com.maeng0830.album.common.service.CommonService;
 import java.net.MalformedURLException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CommonController {
 
-	private final FileDir fileDir;
+	private final CommonService commonService;
 
 	@GetMapping("/images/{storeFileName}")
 	public Resource getImage(@PathVariable String storeFileName) throws MalformedURLException {
-		return new UrlResource("file:" + fileDir.getDir() + storeFileName);
+		return commonService.getImageUrl(storeFileName);
 	}
 }
