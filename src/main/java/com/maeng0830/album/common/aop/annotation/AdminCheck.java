0fa10b793218286a500e9c3edfdd40610334d820
maeng0830 <kmk930830@gmail.com>
2023-11-20 21:29:36 +0900

    Refactor: 로그인 및 관리자 체크 리팩토링(with AOP)   - AOP 적용 기준을 위한 MemberCheck, AdminCheck 애노테이션 생성   - 두 애노테이션에 대한 포인트컷 생성   - 로그인 및 관리자 체크 어드바이스를 위한 LoginCheckAspect 클래스 생성

---
+++ src/main/java/com/maeng0830/album/common/aop/annotation/AdminCheck.java
@@ -0,0 +1,11 @@
+package com.maeng0830.album.common.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AdminCheck {
+}
---
+++ src/main/java/com/maeng0830/album/common/aop/aspect/LoginCheckAspect.java
@@ -0,0 +1,62 @@
+package com.maeng0830.album.common.aop.aspect;
+
+import static com.maeng0830.album.member.domain.MemberRole.ROLE_ADMIN;
+import static com.maeng0830.album.member.exception.MemberExceptionCode.NO_AUTHORITY;
+import static com.maeng0830.album.member.exception.MemberExceptionCode.REQUIRED_LOGIN;
+
+import com.maeng0830.album.common.exception.AlbumException;
+import com.maeng0830.album.security.formlogin.PrincipalDetails;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+
+@Order(2)
+@Aspect
+public class LoginCheckAspect {
+
+	private static final String POINTCUT_PACKAGE = "com.maeng0830.album.common.aop.Pointcuts.";
+
+	@Around(POINTCUT_PACKAGE + "memberCheck()")
+	public Object memberCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		PrincipalDetails principalDetails = getPrincipalDetails(proceedingJoinPoint);
+
+		// 로그인이 안된 경우 예외 발생
+		if (principalDetails == null) {
+			throw new AlbumException(REQUIRED_LOGIN);
+		}
+
+		return proceedingJoinPoint.proceed();
+	}
+
+	@Around(POINTCUT_PACKAGE + "adminCheck()")
+	public Object adminCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		PrincipalDetails principalDetails = getPrincipalDetails(proceedingJoinPoint);
+
+		// 로그인이 안된 경우, 관리자가 아닌 경우 예외 발생
+		if (principalDetails == null) {
+			throw new AlbumException(REQUIRED_LOGIN);
+		} else {
+			if (principalDetails.getMemberDto().getRole() != ROLE_ADMIN) {
+				throw new AlbumException(NO_AUTHORITY);
+			}
+		}
+
+		return proceedingJoinPoint.proceed();
+	}
+
+	private static PrincipalDetails getPrincipalDetails(ProceedingJoinPoint proceedingJoinPoint) {
+		Object[] args = proceedingJoinPoint.getArgs();
+
+		PrincipalDetails principalDetails = null;
+
+		// PrincipalDetails 타입의 인수 조회
+		for (Object arg : args) {
+			if (arg != null && arg.getClass().isInstance(PrincipalDetails.class)) {
+				principalDetails = (PrincipalDetails) arg;
+			}
+		}
+
+		return principalDetails;
+	}
+}
